7ec5e8c59d1874bbe290616aa629c4d8531ffbc2
Laetitia <laetitia@leeroy.ca>
2017-09-21 10:51:31 -0400

    Button reset + random calculation

--- mcgill_robotic/mcgill_robotic.ino
+++ mcgill_robotic/mcgill_robotic.ino
@@ -6,77 +6,95 @@ int secOperand;
 int operatorIndex;
 char operators[2] = {'-', '+'}; 
 String intro[10] = {
-  "Are you dare?",
+  "Do you dare?",
   "More harder now!",
   //"Can you try that? I guess not!"
 };
 
+int result[10];
+int res_length = 0;
+
+int reset = 0;
+int submit = 0;
+
 void setup() {
   //start serial communication
-  //Serial.begin(9600);
+  Serial.begin(9600);
   lcd.begin(16,2);
   randomCalc();
 }
 
 void loop() {
-  // create a local variable to hold the input on pin A0
+  // create a local variable to hold the input on pin A0 - A1
   int btnSet0 = analogRead(A0);
   int btnSet1 = analogRead(A1);
-  int userInput;
   
   // get the value from the set of btns plugged to A0 
   if(btnSet0 == 1023){
-    // play the first frequency in the array on pin 8
-    userInput = 100;
+    result[res_length] = 0;
+    res_length++;
   }
   else if(btnSet0 >= 990 && btnSet0 <= 1010){
-    // play the second frequency in the array on pin 8
-    userInput = 1;
+    result[res_length] = 1;
+    res_length++;
   }
   else if(btnSet0 >= 900 && btnSet0 <= 940){
-    // play the third frequency in the array on pin 8
-    userInput = 2;
+    result[res_length] = 2;
+    res_length++;
   }
   else if(btnSet0 >= 500 && btnSet0 <= 640){
-    // play the fourth frequency in the array on pin 8
-    userInput = 3;
+    result[res_length] = 3;
+    res_length++;
   }
   else if(btnSet0 > 4 && btnSet0 <= 20){
-    // play the fourth frequency in the array on pin 8
-    userInput = 4;
+    result[res_length] = 4;
+    res_length++;
   }
   
   
   // get the value from the set of btns plugged to A1
   if(btnSet1 == 1023){
-    // play the first frequency in the array on pin 8
-    userInput = 5;
+    result[res_length] = 5;
+    res_length++;
   }
   else if(btnSet1 >= 990 && btnSet1 <= 1010){
-    // play the second frequency in the array on pin 8
-    userInput = 6;
+    result[res_length] = 6;
+    res_length++;
   }
   else if(btnSet1 >= 900 && btnSet1 <= 940){
-    // play the third frequency in the array on pin 8
-    userInput = 7;
+    result[res_length] = 7;
+    res_length++;
+    
+    // code for btn reset is placed here because I don't have enough button
+    if (!(reset >= 900 && reset <= 940)) {
+      randomCalc();
+    }
   }
   else if(btnSet1 >= 500 && btnSet1 <= 640){
-    // play the fourth frequency in the array on pin 8
-    userInput = 8;
+    result[res_length] = 8;
+    res_length++;
+    
+    // code for btn submit is placed here because I don't have enough button
+    //if (!(submit >= 500 && submit <= 640)) {
+      computeResult();      
+    //}
   }
   else if(btnSet1 > 4 && btnSet1 <= 20){
-    // play the fourth frequency in the array on pin 8
-    userInput = 9;
+    result[res_length] = 9;
+    res_length++;
   }
   //Serial.println(userInput);
   //Serial.println(btnSet1);
   //Serial.println(btnSet0);
+  reset = btnSet1;
+  submit = btnSet1;
 }
 
-String randomCalc() {  
+void randomCalc() {  
   secOperand = rand() % 100;
   firstOperand = rand() % 100;
   
+  lcd.clear();
   lcd.print(intro[rand() % 2]);
   lcd.setCursor(0, 1);
   lcd.print(firstOperand);
@@ -86,4 +104,12 @@ String randomCalc() {
   lcd.print(secOperand);
   lcd.print(" = ");
 }
+
+void computeResult() {
+  //Serial.println(result);
+  for ( int i = 0; i < res_length; i++ ) {
+    Serial.println(result[i]);
+  }
+}
+
 
